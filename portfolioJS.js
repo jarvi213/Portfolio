f7eaeded6a22f3b6b08eb9e2baf60b0c994dc986
Heather Jarvie <hjarvie@caringbridge.org>
2019-07-29 18:51:38 -0500

    added JS and minor enhancements to mobile nav

--- portfolioJS.js
+++ portfolioJS.js
@@ -1,1 +1,18 @@
 'use strict';
+// When the user scrolls the page, execture stickyScroller
+window.onscroll = function() {stickyScroller()};
+
+// Get the navbar
+var navbar = document.getElementById("navbar");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function stickyScroller() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  };
+};
